84cd3836eda90d24fd39bc997dd8057c491eb9b1
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-07 21:07:05 +0900

    Initial Commit

---
+++ OctoKit/OctoKit.h
@@ -0,0 +1,19 @@
+//
+//  OctoKit.h
+//  OctoKit
+//
+//  Created by 林達也 on 2015/06/07.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OctoKit.
+FOUNDATION_EXPORT double OctoKitVersionNumber;
+
+//! Project version string for OctoKit.
+FOUNDATION_EXPORT const unsigned char OctoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OctoKit/PublicHeader.h>
+
+
